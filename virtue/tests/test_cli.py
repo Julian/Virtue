42dfb5ad7c1e364bb3592bf31e97258abcfc3d0f
Julian Berman <Julian@GrayVines.com>
2014-07-27 16:22:53 +0200

    Pedantry.

--- virtue/tests/test_cli.py
+++ virtue/tests/test_cli.py
@@ -1,3 +1,5 @@
+import os
+
 from virtue import cli
 from virtue.compat import unittest
 
@@ -7,14 +9,39 @@ class TestParser(unittest.TestCase):
         arguments = cli.parse_args(["foo", "bar", "baz"])
         self.assertEqual(arguments["tests"], ["foo", "bar", "baz"])
 
+    def test_it_retrieves_built_in_reporters_by_name(self):
+        arguments = cli.parse_args(["--reporter", "tree", "foo"])
+        self.assertIsInstance(arguments["reporter"], TreeReporter)
+
 
 class TestMain(unittest.TestCase):
+    # TODO: these write to stdout
     def test_it_exits_successfully_for_successful_runs(self):
         with self.assertRaises(SystemExit) as e:
-            cli.main(["virtue.tests.samples.one_successful_test"])
-        self.assertEqual(e.exception.code, 0)
+            cli.main(
+                [
+                    "--reporter", "summary",
+                    "virtue.tests.samples.one_successful_test",
+                ],
+            )
+        self.assertEqual(e.exception.code, os.EX_OK)
 
     def test_it_exits_unsuccessfully_for_unsuccessful_runs(self):
         with self.assertRaises(SystemExit) as e:
-            cli.main(["virtue.tests.samples.one_unsuccessful_test"])
-        self.assertEqual(e.exception.code, 1)
+            cli.main(
+                [
+                    "--reporter", "text",
+                    "virtue.tests.samples.one_unsuccessful_test",
+                ],
+            )
+        self.assertNotEqual(e.exception.code, os.EX_OK)
+
+    def test_it_exits_unsuccessfully_for_unknown_reporters(self):
+        with self.assertRaises(SystemExit) as e:
+            cli.main(
+                [
+                    "--reporter", "non-existent reporter",
+                    "virtue.tests.samples.one_unsuccessful_test",
+                ],
+            )
+        self.assertNotEqual(e.exception.code, os.EX_OK)
