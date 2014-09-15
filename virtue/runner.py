9e2dede94d9e23fee951633bd1ee68aa307333c9
Julian Berman <Julian@GrayVines.com>
2014-09-15 21:59:30 +0100

    Docstring.

--- virtue/runner.py
+++ virtue/runner.py
@@ -3,6 +3,17 @@ from virtue.compat import unittest
 
 
 def run(tests=(), reporter=None):
+    """
+    Run the tests that are loaded by each of the strings provided.
+
+    :argument iterable tests: the collection of tests (specified as
+        :class:`str`\ s) to run
+    :argument Reporter reporter: a :term:`Reporter` to use for the run. If
+        unprovided, the default is to return a :class:`unittest.TestResult`
+        (which produces no output).
+
+    """
+
     if reporter is None:
         reporter = unittest.TestResult()
 
