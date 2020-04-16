111faf258cd8b2f83d0c52346c87b686737efd58
Julian Berman <Julian@GrayVines.com>
2020-04-16 08:12:28 -0400

    vcversioner -> setuptools_scm.

--- virtue/__init__.py
+++ virtue/__init__.py
@@ -1,2 +1,5 @@
-from virtue._version import __version__
-
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+__version__ = metadata.version("virtue")
