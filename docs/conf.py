de8c5528c4856ddf835061b05c52e2703eab46c9
Julian Berman <Julian@GrayVines.com>
2021-05-09 14:44:30 -0400

    Modernized Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,12 @@ version = release.partition("-")[0]
 
 # -- General configuration ---------------------------------------------------
 
+# The reST default role (used for this markup: `text`) to use for all documents
+default_role = "any"
+
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.coverage",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
@@ -37,16 +41,19 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# The reST default role (used for this markup: `text`) to use for all documents
-default_role = "any"
-
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "furo"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,6 +62,17 @@ html_theme = "alabaster"
 
 
 # -- Extension configuration -------------------------------------------------
+
+# -- Options for autodoc extension -------------------------------------------
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+}
+
+# -- Options for autosectionlabel extension ----------------------------------
+
+autosectionlabel_prefix_document = True
 
 # -- Options for intersphinx extension ---------------------------------------
 
